873b669fbf15fb2a634f5f8a60535f61d1fedace
MuraliAirody <muraliairody@gmail.com>
2024-02-27 23:53:00 +0530

    Valid Parentheses

---
+++ 05_String/ValidParentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        boolean ans = sl.isValid("()[]{}");
+
+        System.out.println(ans);
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                char pop = stack.pop();
+
+                if ((pop == '(' && c != ')') ||
+                        (pop == '[' && c != ']') ||
+                        (pop == '{' && c != '}'))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
