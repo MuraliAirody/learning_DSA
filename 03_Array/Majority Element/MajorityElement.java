f2a2e4dcd4fe592bea7be38d99c96dba1fd18c6e
MuraliAirody <muraliairody@gmail.com>
2024-03-04 00:02:08 +0530

    Majority

---
+++ 03_Array/Majority Element/MajorityElement.java
@@ -0,0 +1,29 @@
+
+public class MajorityElement {
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+    // public static int majorityElement(int []v) {
+    //     for(int i=0;i<v.length;i++){
+    //         int count = 0;
+
+    //         for(int j=0;j<v.length;j++){
+    //             if(v[i] == v[j])
+    //              count++;
+    //         }
+
+    //         if(count>v.length/2)
+    //          return v[i];
+    //     }
+    //     return -1;
+
+    // } 
+    public static int majorityElement(int []v) {
+       
+        return -1;
+
+    } 
+}
