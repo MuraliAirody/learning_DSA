5810a4132ded19bbf587663540cf46dffb6ac608
MuraliAirody <muraliairody@gmail.com>
2024-03-12 23:40:31 +0530

    Permutation of String

---
+++ 05_String/PermutationString.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class PermutationString {
+    public static void main(String[] args) {
+      String s ="ABC";
+      Solution obj = new Solution();
+      List<String> ans = obj.find_permutation(s);
+
+      
+      ans.stream().forEach((ss)->System.out.println(ss));
+
+    }
+}
+
+class Solution {
+    public List<String> find_permutation(String S) {
+
+        HashSet<String> set = new HashSet<>();
+        permute(S.toCharArray(), 0, set);
+        
+        ArrayList<String> list = new ArrayList<>(set);
+        
+        Collections.sort(list);
+
+        return list;
+    }
+
+    public static void permute(char[] s, int fi, HashSet<String> set) {
+        if (fi == s.length - 1) {
+            // System.out.println("tt -> "+String.valueOf(s));
+            set.add(String.valueOf(s));
+            return;
+        } else {
+            for (int i = fi; i < s.length; i++) {
+                swap(s, fi, i);
+                permute(s, fi + 1, set);
+                swap(s, fi, i);
+            }
+        }
+    }
+
+    public static void swap(char[] s, int fi, int i) {
+        char temp = s[fi];
+        s[fi] = s[i];
+        s[i] = temp;
+    }
+}
