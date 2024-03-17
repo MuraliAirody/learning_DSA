f45de9ade8f39cf15044f99af8dbb5bd532827b5
MuraliAirody <muraliairody@gmail.com>
2024-03-17 16:02:28 +0530

    Min length

---
+++ 05_String/MinLengthString.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class MinLengthString {
+    public static void main(String[] args) {
+        int ans = Solution.minLength("PPPPPP@PPP@PP$PP");
+
+        System.out.println(ans);
+    }
+}
+
+class Solution{
+    public static int minLength(String s){
+      int minLength = Integer.MAX_VALUE;
+
+      String[] grp = s.split("\\$");
+
+      System.out.println(Arrays.toString(grp));
+
+      for(int i=0;i<grp.length;i++){
+        String[] newGrp = grp[i].split("@");
+
+        for(int j=0;j<newGrp.length;j++)
+          minLength = Math.min(minLength, newGrp[j].length());
+      }
+
+      return minLength;
+
+    }
+
+}
