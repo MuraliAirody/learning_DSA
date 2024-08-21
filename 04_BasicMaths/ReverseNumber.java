713d8967c41ba16eb44377b772db21cf2f1845a8
muralishettigar <muraliairody@gmail.com>
2024-08-21 23:53:05 +0530

    changes

--- 04_BasicMaths/ReverseNumber.java
+++ 04_BasicMaths/ReverseNumber.java
@@ -1,26 +1,36 @@
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        
+
         int num = 1534236469;
         int reverse = 0;
-        while (num>0) {
+        while (num > 0) {
             int rem = num % 10;
-            reverse =  reverse*10 + rem;
-            num = num /10;
-        } 
+            reverse = reverse * 10 + rem;
+            num = num / 10;
+        }
         System.out.println(reverse);
+
+        String ans = new ReverseNumber().reverse(-127);
+        System.out.println(ans);
     }
 
-    public int reverse(int x) {        
+    public String reverse(int x) {
         long reverse = 0;
         int num = x;
-        while (Math.abs(num)>0) {
+        while (Math.abs(num) > 0) {
             int rem = num % 10;
-            reverse =  reverse*10 + rem;
-            num = num /10;
+            reverse = reverse * 10 + rem;
+//            System.out.println(reverse);
+            num = num / 10;
         }
-        
-        return (reverse<Integer.MAX_VALUE && reverse>Integer.MIN_VALUE) ? (int)reverse:0;
+        String reverseString = String.valueOf(Math.abs(reverse));
+        if (x % 10 == 0) {
+            reverseString = '0' + reverseString;
+        }
+        if (x < 0) {
+            reverseString = '-' + reverseString;
+        }
+        return reverseString;
     }
 }
