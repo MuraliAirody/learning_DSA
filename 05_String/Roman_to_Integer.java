831dbb72735a6ac0a9f7a4e5f0d79411e7d97ed0
muralishettigar <muraliairody@gmail.com>
2025-09-13 23:17:54 +0530

    Roman to Int

---
+++ 05_String/Roman_to_Integer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Roman_to_Integer {
+    public static void main(String[] args) {
+        String input = "MCMXCIV";
+        
+        System.out.println(new Roman_to_Integer().romanToInt(input));
+    }
+
+   private int romanToInt(String s) {
+       Map<Character,Integer> map = new HashMap<>();
+       map.put('I',1);
+       map.put('V',5);
+       map.put('X',10);
+       map.put('L',50);
+       map.put('C',100);
+       map.put('D',500);
+       map.put('M',1000);
+       
+    int total = map.get(s.charAt(s.length()-1));
+
+       for(int i=s.length()-1;i>0;i--){
+          if(map.get(s.charAt(i))>map.get(s.charAt(i-1))){
+              total-=map.get(s.charAt(i-1));
+          }else{
+            total+=map.get(s.charAt(i-1));
+
+          }
+        }
+          return total;
+
+    }
+}
