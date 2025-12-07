da8e74def1ff81af834a4502ca08ebd80ee7f4b9
muralishettigar <muraliairody@gmail.com>
2025-12-07 15:38:02 +0530

    Sorted or Not

---
+++ 06_Recursion/medium/SortedOrNot.java
@@ -0,0 +1,18 @@
+public class SortedOrNot {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,8,5};
+        boolean res = sortedOrNot(arr,0);
+        if(res){
+            System.out.println("Sorted array");
+        }else{
+            System.out.println("Unsorted array");
+        }
+    }
+
+    private static boolean sortedOrNot(int[] arr,int index) {
+        if(index==arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1] && sortedOrNot(arr,index+1);
+    }
+}
