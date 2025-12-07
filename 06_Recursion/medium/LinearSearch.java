adc9fdca38f3d3b5fb8980493d999d75da67731b
muralishettigar <muraliairody@gmail.com>
2025-12-07 16:21:18 +0530

    Linear search other logics

--- 06_Recursion/medium/LinearSearch.java
+++ 06_Recursion/medium/LinearSearch.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinearSearch {
+    static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
         int target = 9;
@@ -8,6 +13,17 @@ public class LinearSearch {
         }else{
             System.out.println("Element at: "+pos);
         }
+        int[] arr1 = {1,2,1,3,4,5,6};
+        int target1 = 1;
+        int lpos = linearSearchLastIndex(arr1,target1,arr1.length-1);
+        if(lpos==-1){
+            System.out.println("Not found");
+        }else{
+            System.out.println("Element at: "+lpos);
+        }
+
+        linearSearchAllIndex(new int[]{1, 2, 1, 3, 4, 5, 6},1,0);
+        System.out.println(list);
     }
 
     private static int linearSearch(int[] arr, int target,int i) {
@@ -19,5 +35,27 @@ public class LinearSearch {
         }
         return linearSearch(arr,target,i+1);
     }
+
+    private static int linearSearchLastIndex(int[] arr,int target,int i){
+        if(i<0){
+            return -1;
+        }
+        if(arr[i]==target) {
+            return i;
+        }
+        return linearSearchLastIndex(arr,target,--i);
+    }
+
+    private static void linearSearchAllIndex(int[] arr,int target,int i){
+        if(i>arr.length-1){
+            return;
+        }
+        if(arr[i]==target){
+            list.add(i);
+        }
+
+        linearSearchAllIndex(arr,target,++i);
+    }
+
 
 }
