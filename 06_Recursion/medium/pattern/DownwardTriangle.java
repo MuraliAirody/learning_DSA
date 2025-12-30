239a42cfba148e31b4768fca5107f25958da5fbb
muralishettigar <muraliairody@gmail.com>
2025-12-30 22:51:23 +0530

    normal triangle

--- 06_Recursion/medium/pattern/DownwardTriangle.java
+++ 06_Recursion/medium/pattern/DownwardTriangle.java
@@ -5,6 +5,8 @@ public class DownwardTriangle {
 
         int num = 5;
         downwardTriangle(0,num);
+
+        normalTriangle(0,3);
     }
 
     private static void downwardTriangle(int col,int row){
@@ -18,6 +20,20 @@ public class DownwardTriangle {
         }else{
             System.out.println();
             downwardTriangle(0,row-1);
+        }
+    }
+
+    private static void normalTriangle(int col,int row){
+        if(row==0){
+            return;
+        }
+
+        if(col<row){
+            normalTriangle(col+1,row);
+            System.out.print("* ");
+        }else{
+            normalTriangle(0,row-1);
+            System.out.println();
         }
     }
 }
