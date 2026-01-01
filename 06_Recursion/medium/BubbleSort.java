f299527c76e1d3e01cf99bf170160fd6d882ad43
muralishettigar <muraliairody@gmail.com>
2026-01-01 22:15:50 +0530

    bubble sort

---
+++ 06_Recursion/medium/BubbleSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+       bubbleSort(new int[]{1,7,9,2,3,4});
+
+        int[] arr = {1,7,9,2,3,4};
+        recursionBubbleSort(arr,0,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    private static void bubbleSort(int[] arr){
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<(arr.length-1-i);j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void recursionBubbleSort(int[] arr,int i,int j){
+
+        if(i>=arr.length){
+            return;
+        }
+        if(j<arr.length-1-i) {
+            if ((arr[j] > arr[j + 1])) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+            recursionBubbleSort(arr, i, j + 1);
+        }else{
+           recursionBubbleSort(arr, i+1,0);
+        }
+    }
+}
