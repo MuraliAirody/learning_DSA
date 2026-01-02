07fc1f74d95a4e26238f61e2cdd6c1c418352be5
muralishettigar <muraliairody@gmail.com>
2026-01-03 00:55:10 +0530

    merge sort by returning array

--- 06_Recursion/medium/MergeSort.java
+++ 06_Recursion/medium/MergeSort.java
@@ -9,6 +9,10 @@ public class MergeSort {
 
         System.out.println(Arrays.toString(arr));
 
+
+        int[] arr1 = {5, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(mergeSort1(arr1)));
+
     }
 
     private static void mergeSort(int[] arr,int start,int end) {
@@ -54,4 +58,52 @@ public class MergeSort {
            arr[start+i] = res.get(i);
        }
     }
+
+    private static int[] mergeSort1(int[] arr) {
+        if(arr.length==1){
+            return arr;
+        }
+
+        int mid = arr.length/2;
+
+        int[] left = mergeSort1(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergeSort1(Arrays.copyOfRange(arr,mid,arr.length));
+
+        return  merge1(left,right);
+    }
+
+    private static int[] merge1(int[] first,int[] second) {
+        int[] res = new int[first.length+second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i<first.length && j<second.length){
+            if(first[i]<second[j]){
+                res[k] = first[i];
+                i++;
+            }else{
+                res[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<first.length){
+            res[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while(j<second.length){
+            res[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return res;
+    }
+
+
 }
